ab91d3cc5b58253290fa2e21b61c589633dd8560
Ali A. Almiskeen <alimiskeen@me.com>
2022-09-19 20:53:07 -0500

    trying a function

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const Query = require("minecraft-query");
+// const express = require("express");
+// const app = express();
+// const port = 80;
 
+const Query = require("minecraft-query");
 const q = new Query({ host: "localhost", port: 9630, timeout: 7500 });
 
-q.fullStat()
-  .then((success) => {
-    console.log(success);
+// q.fullStat()
+//   .then((success) => {
+//     console.log(success);
 
-    return q.basicStat();
-  })
+//     return q.basicStat();
+//   })
 
-  .then((success) => {
-    console.log(success);
+//   .then((success) => {
+//     console.log(success);
 
-    q.close();
+//     q.close();
+//   });
+
+// app.get("/", (req, res) => {});
+
+function getServerInfo() {
+  var serverInformation;
+
+  q.fullStat().then((success) => {
+    serverInformation = success;
+    return;
   });
+
+  return serverInformation;
+}
+
+console.log(getServerInfo());
