0f3b2d3eeff19cd98bb8b5c509c656164ece19a6
Ali A. Almiskeen <alimiskeen@me.com>
2022-09-19 21:02:51 -0500

    yes

--- index.js
+++ index.js
@@ -21,20 +21,7 @@ const q = new Query({ host: "localhost", port: 9630, timeout: 7500 });
 // app.get("/", (req, res) => {});
 
 function getServerInfo() {
-  var serverInformation;
-
-  q.fullStat()
-    .then((success) => {
-      console.log(success);
-      serverInformation = success;
-      return q.basicStat();
-    })
-    .then((success) => {
-      console.log(success);
-      q.close();
-    });
-
-  return serverInformation;
+  return q.fullStat();
 }
 
 console.log(getServerInfo());
