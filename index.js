6ebda73e946e3f6a98a180b19e318af32555fb1d
Ali A. Almiskeen <alimiskeen@me.com>
2022-09-19 21:44:00 -0500

    wo0w

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-// const express = require("express");
-// const app = express();
-// const port = 80;
+const express = require("express");
+const app = express();
+const port = 80;
 
 const Query = require("minecraft-query");
-// const q = new Query({ host: "localhost", port: 9630, timeout: 7500 });
+const q = new Query({ host: "localhost", port: 9630, timeout: 7500 });
 
-// q.fullStat()
-//   .then((success) => {
-//     console.log(success);
+q.fullStat()
+  .then((success) => {
+    console.log(success);
 
-//     return q.basicStat();
-//   })
+    return q.basicStat();
+  })
 
-//   .then((success) => {
-//     console.log(success);
+  .then((success) => {
+    console.log(success);
 
-//     q.close();
-//   });
-
-// app.get("/", (req, res) => {});
-
-function getServerInfo() {
-  const q = new Query({ host: "localhost", port: 9630, timeout: 7500 });
-  return (statPromise = q.fullStat().then((success) => {
-    resolve(success);
     q.close();
-  }));
-}
+  });
 
-var stats = await getServerInfo();
-console.log(stats);
+app.get("/", (req, res) => {
+  const q = new Query({ host: "localhost", port: 9630, timeout: 7500 });
+  q.fullStat().then((success) => {
+    console.log(success);
+    q.close();
+  });
+  res.send("look in the terminal. is it there?");
+});
+
+app.listen(port, () => {
+  console.log("started server on port: ${port}");
+});
