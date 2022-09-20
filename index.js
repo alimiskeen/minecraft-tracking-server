1d6af3a7b5359aa029673e6c50c8dc50a21fc2e6
Ali A. Almiskeen <alimiskeen@me.com>
2022-09-19 21:45:51 -0500

    wo0w4

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const app = express();
 const port = 80;
 
 const Query = require("minecraft-query");
-const q = new Query({ host: "localhost", port: 9630, timeout: 7500 });
+// const q = new Query({ host: "localhost", port: 9630, timeout: 7500 });
 
-q.fullStat()
-  .then((success) => {
-    console.log(success);
+// q.fullStat()
+//   .then((success) => {
+//     console.log(success);
 
-    return q.basicStat();
-  })
+//     return q.basicStat();
+//   })
 
-  .then((success) => {
-    console.log(success);
+//   .then((success) => {
+//     console.log(success);
 
-    q.close();
-  });
+//     q.close();
+//   });
 
 app.get("/", (req, res) => {
   const q = new Query({ host: "localhost", port: 9630, timeout: 7500 });
